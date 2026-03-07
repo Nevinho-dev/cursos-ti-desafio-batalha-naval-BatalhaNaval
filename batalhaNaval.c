018982fa66d03c1d0efb09917e194121d5f38ade
JoaoPedro-dev <joaopedro.juventinoalves10@gmail.com>
2026-03-07 14:17:53 -0300

    Criei a matrix que representa o tabuleiro, e inseri os elementos "3" para representar os navios; em seguida usei printf para localizar cada parte e apresentar ao usuário.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,32 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int tabuleiro[10][10] = {
+    {0,0,0,0,3,3,3,3,3,0,},
+    {0,0,0,0,0,0,0,0,0,0,},
+    {0,0,0,0,0,0,0,0,0,0,},
+    {0,0,0,0,0,0,0,0,0,0,},
+    {0,0,0,3,0,0,0,0,0,0,},
+    {0,0,0,3,0,0,0,0,0,0,},
+    {0,0,0,3,0,0,0,0,0,0,},
+    {0,0,0,3,0,0,0,0,0,0,},
+    {0,0,0,3,0,0,0,0,0,0,},
+    {0,0,0,3,0,0,0,0,0,0,}
+};
+
+printf("Navio 1 (Horizontal):\n");
+printf("Parte 1: (0,4)\n");
+printf("Parte 2: (0,5)\n");
+printf("Parte 3: (0,6)\n");
+printf("Parte 4: (0,7)\n");
+printf("parte 5: (0,8)\n");
+printf("\n");
+printf("Navio 2 (Vertical):\n");
+printf("Parte 1: (4,3)\n");
+printf("Parte 2: (5,3)\n");
+printf("Parte 3: (6,3)\n");
+printf("Parte 4: (7,3)\n");
+printf("Parte 5: (8,3)\n");
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
